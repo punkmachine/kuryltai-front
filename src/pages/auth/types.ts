29910649521449c552fb91591e2143c0f419cf86
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-04-14 14:56:08 +0600

    add: user types

---
+++ src/pages/auth/types.ts
@@ -0,0 +1,8 @@
+export enum AuthStepEnum {
+  signInEmail = 'sign-in-email',
+  signInPassword = 'sign-in-password',
+  resetPassword = 'reset-password',
+  resetPasswordAfterSendEmail = 'reset-password-after-send-email',
+  newPassword = 'new-password',
+  signUp = 'sign-up',
+};
---
+++ src/types/backend/user/user.payload.ts
@@ -0,0 +1,34 @@
+export interface IPayloadLogin {
+  email: string,
+  password: string,
+}
+
+export interface IPayloadResetPass {
+  password: string,
+  confirm_password: string,
+
+  // "profile_uuid": "b281c520-6031-4369-9703-fb79e6d38688",
+  // "security_token": "534d4994-7424-423e-9e5f-79aa402ec699e6a817ec-7bb5-4d43-a290-6f3ef51d27be",
+}
+
+export interface IPayloadSignUp {
+  email: string,
+  password: string,
+  confirm_password: string,
+}
+
+export interface IPayloadCheckEmail {
+  email: string,
+}
+
+export interface IPayloadGetAccessByRefresh {
+  refresh: string,
+}
+
+export interface IPayloadLogout {
+  refresh: string,
+}
+
+export interface IPayloadForgetEmail {
+  email: string,
+}
