1c63b57e778c53b373e1472023496f3dc10462cf
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-04-22 04:52:01 +0600

    add: UIAvatar

---
+++ src/components/ui/UIAvatar.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    :src="src"
+    class="h-9 w-9 rounded-full object-cover"
+  />
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface IProps {
+  src: string;
+}
+
+defineProps<IProps>();
+</script>
