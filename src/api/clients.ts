070959a3e02cfd4f40cf45576a30b85f2781380f
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-04-21 11:26:48 +0600

    add: error response interceptor

--- src/api/clients.ts
+++ src/api/clients.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, { type AxiosResponse, type AxiosError } from 'axios';
+import { toast } from 'vue3-toastify';
 // import Cookies from 'js-cookie';
 
 // import type { InternalAxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
@@ -8,7 +9,21 @@ import axios from 'axios';
 // import { api } from '.';
 
 const client = axios.create({
-  baseURL: 'http://kuryltai.kz/api/v0',
+  baseURL: 'https://api-kuryltai.kz/api/v0',
 });
+
+function successResponse(data: AxiosResponse) {
+  return data.data;
+}
+
+function errorResponse(error: AxiosError<{ error: { message: string } }>) {
+  if (error.response?.data?.error?.message) {
+    toast.error(error.response.data.error.message);
+  }
+
+  throw error;
+}
+
+client.interceptors.response.use(successResponse, errorResponse);
 
 export { client };
