d3be13d7b67ca4a87ce90c316225537cc6c9f2a9
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-04-23 05:38:37 +0600

    add: header в mainlayout

---
+++ src/components/layout/main/MainHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div class="mb-1 h-8 w-[122px] cursor-pointer">
+        <img
+          src="@/assets/icons/logo.svg"
+          alt=""
+        />
+      </div>
+
+      <div>
+        <UIInput
+          v-model="search"
+          placeholder="Поиск"
+          class="w-96"
+        />
+      </div>
+
+      <div class="cursor-pointer">
+        <div class="flex items-center gap-1.5">
+          <UIAvatar src="https://avatars.githubusercontent.com/u/76869388?v=4" />
+
+          <span class="text-sm font-medium text-gray-600">Алекс</span>
+
+          <div>
+            <svg class="h-5 w-5">
+              <use xlink:href="@/assets/icons/sprites/btns.svg#arrow"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import UIInput from '@/components/ui/UIInput.vue';
+import UIAvatar from '@/components/ui/UIAvatar.vue';
+
+const search = ref<string>('');
+</script>
+
+<style scoped>
+.header {
+  @apply fixed top-0 flex w-full justify-center bg-white;
+
+  height: var(--header-height);
+}
+
+.header__content {
+  @apply flex w-full max-w-7xl items-center justify-between py-3;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,26 @@
 <template>
-  <main class="main-layout">
-    <slot></slot>
-  </main>
+  <div class="main-scope">
+    <MainHeader />
+
+    <main class="main-layout">
+      <slot></slot>
+    </main>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import MainHeader from '@/components/layout/main/MainHeader.vue';
+</script>
 
-<style scoped></style>
+<style scoped>
+.main-scope {
+  --header-height: 60px;
+}
+
+.main-layout {
+  @apply bg-gray-200;
+
+  margin-top: var(--header-height);
+  min-height: calc(100vh - var(--header-height));
+}
+</style>
