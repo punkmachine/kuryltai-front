dd82688dfdb3ee288183afd37d84deaaf9eb6bf7
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-09 08:58:05 +0500

    add: sidebar

---
+++ src/components/layout/main/MainSidebar.vue
@@ -0,0 +1,74 @@
+<template>
+  <aside>
+    <RouterLink
+      to="/"
+      class="flex items-center gap-1 w-full px-3 py-2"
+      :class="{
+        'rounded-md bg-gray-50': activeLink === '',
+      }"
+    >
+      <div>
+        <svg class="w-4 h-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#profile"></use>
+        </svg>
+      </div>
+      <span>
+        Мой профиль
+      </span>
+    </RouterLink>
+
+    <RouterLink
+      to="/todo"
+      class="flex items-center gap-1 w-full px-3 py-2"
+      :class="{
+        'rounded-md bg-gray-50': activeLink === '/todo',
+      }"
+    >
+      <div>
+        <svg class="w-4 h-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#subscriptions"></use>
+        </svg>
+      </div>
+      <span>
+        Подписки
+      </span>
+    </RouterLink>
+
+    <RouterLink
+      to="/settings"
+      class="flex items-center gap-1 w-full px-3 py-2"
+      :class="{
+        'rounded-md bg-gray-50': activeLink === '/settings',
+      }"
+    >
+      <div>
+        <svg class="w-4 h-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#settings"></use>
+        </svg>
+      </div>
+      <span>
+        Настройки
+      </span>
+    </RouterLink>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+import { watch, ref } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+
+const route = useRoute();
+const activeLink = ref('');
+
+watch(() => route.path, () => {
+  activeLink.value = route.path;
+});
+</script>
+
+<style scoped>
+aside {
+  @apply bg-white flex px-3 py-6 flex-col items-start gap-1 rounded-lg;
+
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10);
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,13 +3,20 @@
     <MainHeader />
 
     <main class="main-layout">
-      <slot></slot>
+      <div class="main-layout__wrapper">
+        <div class="main-sidebar">
+          <MainSidebar />
+        </div>
+
+        <slot></slot>
+      </div>
     </main>
   </div>
 </template>
 
 <script lang="ts" setup>
 import MainHeader from '@/components/layout/main/MainHeader.vue';
+import MainSidebar from '@/components/layout/main/MainSidebar.vue';
 </script>
 
 <style scoped>
@@ -18,9 +25,22 @@ import MainHeader from '@/components/layout/main/MainHeader.vue';
 }
 
 .main-layout {
-  @apply bg-gray-200;
+  @apply bg-gray-200 flex justify-center;
 
   margin-top: var(--header-height);
   min-height: calc(100vh - var(--header-height));
+}
+
+.main-layout__wrapper {
+  @apply grid mt-6 gap-x-5;
+  grid-template-columns: 292px 912px;
+}
+
+.main-sidebar {
+  @apply w-[292px];
+}
+
+main {
+  min-width: 100%;
 }
 </style>
