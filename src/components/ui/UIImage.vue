4e47ec58e785fc663e7137161668d5b738cc754e
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-10 06:26:00 +0500

    add: UIImage component

---
+++ src/components/ui/UIImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="overflow-hidden rounded-lg">
+    <img
+      :src="src"
+      :alt="alt ?? ''"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface IProps {
+  src: string;
+  alt?: string;
+}
+
+defineProps<IProps>();
+</script>
+
+<style scoped></style>
