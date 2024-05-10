46db008caf6fb02494ad1a5a068bbc44dd23b847
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-10 06:26:13 +0500

    add: UIVideo component

---
+++ src/components/ui/UIVideo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="overflow-hidden rounded-lg">
+    <video
+      :src="src"
+      controls
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface IProps {
+  src: string;
+}
+
+defineProps<IProps>();
+</script>
+
+<style scoped></style>
