c4301e69664279f2c14dd6c67778eeadb41b4f9c
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-10 14:46:30 +0500

    add: UITag component

---
+++ src/components/ui/UITag.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tag">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.tag {
+  @apply flex h-6 items-center justify-center rounded bg-blue-gray-50 px-2 py-1 text-xs font-bold;
+}
+</style>
