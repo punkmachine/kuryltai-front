84ec6e24f927ada06f95fdbe768f8839cbd1ba45
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-11 06:37:02 +0500

    add: ui modal component

---
+++ src/components/ui/UIModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <teleport
+    to="body"
+    :disabled="visible"
+  >
+    <div v-if="visible">
+      <div
+        class="overlay"
+        @click="$emit('close')"
+      ></div>
+
+      <article
+        v-bind="$attrs"
+        class="modal"
+      >
+        <div
+          class="fixed right-4 top-4 cursor-pointer"
+          @click="$emit('close')"
+        >
+          <svg class="h-6 w-6">
+            <use xlink:href="@/assets/icons/sprites/btns.svg#close"></use>
+          </svg>
+        </div>
+
+        <div class="modal__header">
+          <slot name="header"></slot>
+        </div>
+
+        <div class="modal__body">
+          <slot name="body"></slot>
+        </div>
+
+        <div class="modal__footer">
+          <slot name="footer"></slot>
+        </div>
+      </article>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+interface IProps {
+  visible: boolean;
+}
+
+interface IEmits {
+  (e: 'close'): void;
+}
+
+defineProps<IProps>();
+defineEmits<IEmits>();
+</script>
+
+<style scoped>
+.overlay {
+  @apply fixed left-0 top-0 z-40 h-screen w-screen bg-black opacity-30;
+}
+
+.modal {
+  @apply fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-7 rounded-lg bg-white px-5 py-6;
+}
+</style>
