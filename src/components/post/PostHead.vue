d59bfa288b51e14f82258a7cb8c96e7975f8c1a2
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-12 07:14:59 +0500

    add: post head component

--- src/components/post/PostHead.vue
+++ src/components/post/PostHead.vue
@@ -12,16 +12,54 @@
       </div>
     </div>
 
-    <div class="flex h-6 w-6 items-center justify-center">
-      <svg class="h-4 w-4">
-        <use xlink:href="@/assets/icons/sprites/btns.svg#dotted"></use>
-      </svg>
+    <div class="relative">
+      <button
+        class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-lg"
+        :class="{
+          'bg-purple-800': activeOptions,
+        }"
+        @click="activeOptions = !activeOptions"
+      >
+        <svg
+          class="h-4 w-4 stroke-blue-gray-300"
+          :class="{
+            'stroke-white': activeOptions,
+          }"
+        >
+          <use xlink:href="@/assets/icons/sprites/btns.svg#dotted"></use>
+        </svg>
+      </button>
+
+      <div
+        class="absolute right-0 top-8 z-10 flex-col gap-1 rounded-lg bg-white p-3 shadow-xl"
+        :class="{
+          hidden: !activeOptions,
+          flex: activeOptions,
+        }"
+      >
+        <template v-if="isMyPost">
+          <button
+            class="rounded-md p-2 text-left text-sm font-medium text-blue-gray-500 hover:bg-blue-50"
+            @click="$emit('delete-post')"
+          >
+            Удалить
+          </button>
+        </template>
+        <template v-else>
+          <button class="rounded-md p-2 text-left text-sm font-medium text-blue-gray-500 hover:bg-blue-50">
+            Поделиться
+          </button>
+          <button class="rounded-md p-2 text-left text-sm font-medium text-blue-gray-500 hover:bg-blue-50">
+            Пожаловаться
+          </button>
+        </template>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, computed } from 'vue';
+import { defineProps, computed, ref, defineEmits } from 'vue';
 import UIAvatar from '@/components/ui/UIAvatar.vue';
 import { getTimeHasPassed } from '@/helpers/getTimeHasPassed.ts';
 
@@ -29,13 +67,19 @@ interface IProps {
   date: string;
   name: string;
   avatar: string;
+  isMyPost?: boolean;
+}
+
+interface IEmits {
+  (e: 'delete-post'): void;
 }
 
 const props = defineProps<IProps>();
+defineEmits<IEmits>();
+
+const activeOptions = ref(false);
 
 const readableDate = computed(() => {
   return getTimeHasPassed(props.date);
 });
 </script>
-
-<style scoped></style>
