34095a7ba72de31b007d34425a11451d449e7398
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-13 05:13:18 +0500

    add: ui upload component

---
+++ src/components/ui/UIUpload.vue
@@ -0,0 +1,10 @@
+<template>
+  <article class="upload">
+    <svg>
+      <use xlink:href="@/assets/icons/sprites/inputs.svg#image"></use>
+    </svg>
+    <span>Выбрать файл</span>
+  </article>
+</template>
+
+<script lang="ts" setup></script>
