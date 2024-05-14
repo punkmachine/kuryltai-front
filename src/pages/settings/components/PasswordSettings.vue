d4a520a540a1bb3cd79d7e86837374205facdb41
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-14 05:18:42 +0500

    add: password settings component in settings page

---
+++ src/pages/settings/components/PasswordSettings.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <form
+      @click.prevent="() => {}"
+      class="flex flex-col gap-3.5"
+    >
+      <UIInput
+        v-model="passwordData.currentPassword"
+        type="password"
+        id="current-password"
+        placeholder="*********"
+        required
+        label="Текущий пароль"
+      >
+        <template #inner-icon-right>
+          <span
+            class="input__inner-icon-right cursor-pointer"
+            @click="changeTypeInput('current-password')"
+          >
+            <svg>
+              <use xlink:href="@/assets/icons/sprites/inputs.svg#eye-close"></use>
+            </svg>
+          </span>
+        </template>
+      </UIInput>
+
+      <UIInput
+        v-model="passwordData.newPassword"
+        type="password"
+        id="new-password"
+        placeholder="*********"
+        required
+        label="Новый пароль"
+      >
+        <template #inner-icon-right>
+          <span
+            class="input__inner-icon-right cursor-pointer"
+            @click="changeTypeInput('new-password')"
+          >
+            <svg>
+              <use xlink:href="@/assets/icons/sprites/inputs.svg#eye-close"></use>
+            </svg>
+          </span>
+        </template>
+      </UIInput>
+
+      <UIInput
+        v-model="passwordData.repeatPassword"
+        type="password"
+        id="repeat-password"
+        placeholder="*********"
+        required
+        label="Повторите пароль"
+      >
+        <template #inner-icon-right>
+          <span
+            class="input__inner-icon-right cursor-pointer"
+            @click="changeTypeInput('repeat-password')"
+          >
+            <svg>
+              <use xlink:href="@/assets/icons/sprites/inputs.svg#eye-close"></use>
+            </svg>
+          </span>
+        </template>
+      </UIInput>
+
+      <button
+        type="submit"
+        class="btn btn--primary mt-4 w-max px-10 uppercase"
+      >
+        Сохранить
+      </button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import UIInput from '@/components/ui/UIInput.vue';
+
+const passwordData = ref({
+  currentPassword: '',
+  newPassword: '',
+  repeatPassword: '',
+});
+
+function changeTypeInput(inputId: string) {
+  const input: HTMLInputElement | null = document.querySelector(`input#${inputId}`);
+
+  if (input) {
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
+  }
+}
+</script>
