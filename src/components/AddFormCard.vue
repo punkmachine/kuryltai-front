63d5ac162d4364da2799eec9a3b2f63c24d24323
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-15 05:09:38 +0500

    add: form credit card component

---
+++ src/components/AddFormCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <UIInput
+      v-model="cardData.number"
+      label="Номер карты"
+      placeholder="0000 0000 0000 0000"
+      required
+    />
+
+    <div class="flex items-center gap-3">
+      <UIInput
+        v-model="cardData.date"
+        label="Срок действия"
+        placeholder="00/00"
+        required
+      />
+
+      <UIInput
+        v-model="cardData.cvc"
+        label="CVC"
+        placeholder="000"
+        required
+      />
+    </div>
+
+    <UIInput
+      v-model="cardData.name"
+      label="Имя на карте"
+      placeholder="Имя"
+      required
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch, defineEmits, defineProps, onMounted, toRaw } from 'vue';
+import UIInput from '@/components/ui/UIInput.vue';
+import type { ICard } from '@/types/types';
+
+interface IProps {
+  initialCardData: ICard;
+}
+
+interface IEmits {
+  (e: 'update-card-data', data: ICard): void;
+}
+
+const emit = defineEmits<IEmits>();
+const props = defineProps<IProps>();
+
+const cardData = ref<ICard>({
+  name: '',
+  cvc: '',
+  date: '',
+  number: '',
+});
+
+watch(
+  () => cardData,
+  () => {
+    emit('update-card-data', cardData.value);
+  },
+);
+
+watch(
+  () => props.initialCardData,
+  () => {
+    if (toRaw(cardData.value) !== toRaw(props.initialCardData)) {
+      cardData.value = props.initialCardData;
+    }
+  },
+);
+
+onMounted(() => {
+  cardData.value = props.initialCardData;
+});
+</script>
--- src/pages/settings/view.vue
+++ src/pages/settings/view.vue
@@ -1,3 +1,112 @@
 <template>
-  <main>page</main>
+  <section class="settings">
+    <div class="settings__content">
+      <keep-alive>
+        <component :is="component" />
+      </keep-alive>
+    </div>
+
+    <div class="settings__additional">
+      <div
+        class="flex cursor-pointer items-center gap-1 rounded-lg px-3 py-2"
+        :class="{
+          'bg-blue-gray-50': settingState === 'account',
+        }"
+        @click="settingState = 'account'"
+      >
+        <svg class="h-4 w-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#account"></use>
+        </svg>
+        <span class="text-sm font-medium text-blue-gray-500">Аккаунт</span>
+      </div>
+
+      <div
+        class="flex cursor-pointer items-center gap-1 rounded-lg px-3 py-2"
+        :class="{
+          'bg-blue-gray-50': settingState === 'password',
+        }"
+        @click="settingState = 'password'"
+      >
+        <svg class="h-4 w-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#password"></use>
+        </svg>
+        <span class="text-sm font-medium text-blue-gray-500">Пароль</span>
+      </div>
+
+      <div
+        class="flex cursor-pointer items-center gap-1 rounded-lg px-3 py-2"
+        :class="{
+          'bg-blue-gray-50': settingState === 'payment',
+        }"
+        @click="settingState = 'payment'"
+      >
+        <svg class="h-4 w-4">
+          <use xlink:href="@/assets/icons/sprites/menu.svg#payment"></use>
+        </svg>
+        <span class="text-sm font-medium text-blue-gray-500">Способ оплаты</span>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script lang="ts" setup>
+import { ref, computed, onMounted, watch, onBeforeUnmount } from 'vue';
+import AccountSettings from './components/AccountSettings.vue';
+import PasswordSettings from './components/PasswordSettings.vue';
+import PaymentSettings from './components/PaymentSettings.vue';
+
+type TSettingState = 'account' | 'password' | 'payment';
+
+const settingState = ref<TSettingState>('account');
+
+const component = computed(() => {
+  if (settingState.value === 'account') {
+    return AccountSettings;
+  } else if (settingState.value === 'password') {
+    return PasswordSettings;
+  } else {
+    return PaymentSettings;
+  }
+});
+
+watch(
+  () => settingState.value,
+  () => {
+    localStorage.setItem('settings-state', settingState.value);
+  },
+);
+
+onMounted(() => {
+  if (localStorage.getItem('settings-state')) {
+    settingState.value = localStorage.getItem('settings-state') as TSettingState;
+  }
+});
+
+onBeforeUnmount(() => {
+  localStorage.removeItem('settings-state');
+});
+</script>
+
+<style scoped>
+.settings {
+  @apply grid gap-x-5;
+
+  grid-template-columns: 2fr 1fr;
+}
+
+.settings__content {
+  @apply w-[604px] rounded-lg bg-white px-6 py-3;
+
+  box-shadow:
+    0px 1px 3px 0px rgba(0, 0, 0, 0.1),
+    0px 1px 2px -1px rgba(0, 0, 0, 0.1);
+}
+
+.settings__additional {
+  @apply flex h-max w-[292px] flex-col rounded-lg bg-white px-3 py-6;
+
+  box-shadow:
+    0px 1px 3px 0px rgba(0, 0, 0, 0.1),
+    0px 1px 2px -1px rgba(0, 0, 0, 0.1);
+}
+</style>
