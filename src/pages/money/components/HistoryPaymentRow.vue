b7dbd824a9935e6caac801ec365e954e22b71a73
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-23 05:28:16 +0500

    add: history payments page

---
+++ src/pages/money/components/HistoryPaymentRow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="border-b border-solid border-gray-300 py-3">
+    <div class="text-sm font-medium text-gray-900">{{ id }}</div>
+    <div class="text-sm text-blue-gray-500">{{ sum }}</div>
+    <div class="text-sm text-blue-gray-500">{{ date }}</div>
+    <div
+      class="text-sm"
+      :class="{
+        'text-amber-500': status === 1,
+        'text-green-500': status === 2,
+        'text-red-500': status === 3,
+      }"
+    >
+      {{ readableStatus }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from 'vue';
+
+interface IProps {
+  id: string;
+  sum: string;
+  date: string;
+  status: 1 | 2 | 3;
+}
+
+const props = defineProps<IProps>();
+
+const readableStatus = computed(() => {
+  const dict = {
+    1: 'Ожидает',
+    2: 'Готово',
+    3: 'Ошибка',
+  };
+
+  return dict[props.status];
+});
+</script>
---
+++ src/pages/money/components/HistoryPayments.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="table-grid items-center border border-solid border-gray-300 bg-gray-100 px-4 py-3">
+      <span class="text-xs font-medium text-blue-gray-500">№ заявки</span>
+      <span class="text-xs font-medium text-blue-gray-500">Сумма, тг</span>
+      <span class="text-xs font-medium text-blue-gray-500">Дата транзакции</span>
+      <span class="text-xs font-medium text-blue-gray-500">Статус</span>
+    </div>
+
+    <div class="flex flex-col">
+      <HistoryPaymentRow
+        class="table-grid"
+        id="12345"
+        :sum="getFormatSum('9999999')"
+        :date="dayjs().format('DD.MM.YYYY, HH:MM')"
+        :status="1"
+      />
+
+      <HistoryPaymentRow
+        class="table-grid"
+        id="12345"
+        :sum="getFormatSum('9999999')"
+        :date="dayjs().format('DD.MM.YYYY, HH:MM')"
+        :status="2"
+      />
+
+      <HistoryPaymentRow
+        class="table-grid"
+        id="12345"
+        :sum="getFormatSum('9999999')"
+        :date="dayjs().format('DD.MM.YYYY, HH:MM')"
+        :status="3"
+      />
+    </div>
+
+    <div class="mt-6 flex justify-end">
+      <UIPagination
+        :current-page="1"
+        :count-pages="10"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs';
+import UIPagination from '@/components/ui/UIPagination.vue';
+import HistoryPaymentRow from './HistoryPaymentRow.vue';
+import { getFormatSum } from '@/helpers/getFormatSum';
+</script>
+
+<style scoped>
+.table-grid {
+  @apply grid;
+  grid-template-columns: 1fr 1fr 2fr 1fr;
+}
+</style>
