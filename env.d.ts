6a042d86246d44518bd4aaef06ce4ef1f3058edd
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-05-23 05:27:51 +0500

    add: income money row component

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,6 @@ interface ImportMeta {
     VITE_APP_GOOGLE_CLIENT_ID: string;
     VITE_APP_GOOGLE_REDIRECT_URL: string;
     VITE_APP_GOOGLE_CLIENT_SECRET: string;
+    VITE_APP_API_URL: string;
   }
 }
---
+++ src/pages/money/components/IncomeMoneyRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border-b border-solid border-gray-300 py-3">
+    <div class="text-sm text-gray-900">{{ login }}</div>
+    <div class="text-sm text-blue-gray-500">{{ paymentType }}</div>
+    <div class="text-sm text-blue-gray-500">{{ date }}</div>
+    <div class="text-sm text-blue-gray-500">{{ sum }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface IProps {
+  login: string;
+  paymentType: string; // @todo: enum
+  date: string;
+  sum: string;
+}
+
+defineProps<IProps>();
+</script>
