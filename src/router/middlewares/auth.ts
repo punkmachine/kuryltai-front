9644a5eda455956b05d002e5ac3bbb47a224bfb3
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-06-08 16:27:30 +0500

    add: fetchers profile data in middleware

--- src/router/middlewares/auth.ts
+++ src/router/middlewares/auth.ts
@@ -1,15 +1,26 @@
 import router from '@/router';
 import Cookies from 'js-cookie';
 import { authCookies } from '@/entities/constants/browserStoreKeys';
+import { useMyProfileStore } from '@/store';
 
+function fetchersProfile() {
+  const store = useMyProfileStore();
+
+  store.fetchMyCards();
+  store.fetchMyProfile();
+}
+
+// eslint-disable-next-line max-lines-per-function
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!Cookies.get(authCookies.access)) {
       next('/auth');
     } else {
+      fetchersProfile();
       next();
     }
   } else {
+    fetchersProfile();
     next();
   }
 });
